797522c08ceeefc14ba54ab14eaf2ead35d3d22e
koni-hub <delgadoargie04z@gmail.com>
2024-12-11 22:31:01 +0800

    chore: add inquiry features

--- public_html/topbar.php
+++ public_html/topbar.php
@@ -1,4 +1,4 @@
-<?php include ('./db_connect.php'); ?>
+<?php include('./db_connect.php'); ?>
 <style>
   .logo {
     margin: auto;
@@ -7,6 +7,37 @@
     padding: 7px 11px;
     border-radius: 50% 50%;
     color: #000000b3;
+  }
+
+  .badge-notify {
+    font-size: 0.8rem;
+    padding: 0.3rem 0.6rem;
+    color: white;
+    background-color: #f44336;
+  }
+
+  .position-relative {
+    position: relative;
+  }
+
+  .position-absolute {
+    position: absolute;
+  }
+
+  .top-0 {
+    top: 0;
+  }
+
+  .start-100 {
+    left: 100%;
+  }
+
+  .translate-middle {
+    transform: translate(-50%, -50%);
+  }
+
+  .badge-rounded-pill {
+    border-radius: 50rem;
   }
 </style>
 
@@ -22,33 +53,34 @@
           </b></large>
       </div>
       <div class="float-right d-flex align-items-center">
-      <!-- Notification Icon -->
-      <div class="mr-4">
-        <a href="index.php?page=inquire" class="text-white">
-          <i class="fa fa-bell"></i>
-          <span class="badge-txt"><?php echo $conn->query("SELECT * FROM inquire")->num_rows ?></span>
-        </a>
+        <!-- Notification Icon -->
+        <div class="mr-4">
+          <a href="index.php?page=inquire" class="text-white position-relative">
+            <i class="fa fa-bell"></i>
+            <span class="badge-notify position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+              <?php echo $conn->query("SELECT * FROM inquire")->num_rows ?>
+            </span>
+          </a>
+        </div>
+        <!-- Account Settings Dropdown (Login Name) -->
+        <div class="dropdown">
+          <a href="#" class="text-white dropdown-toggle" id="account_settings" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            <?php echo $_SESSION['login_name']; ?>
+          </a>
+          <div class="dropdown-menu" aria-labelledby="account_settings" style="left: -2.5em;">
+            <a class="dropdown-item" href="javascript:void(0)" id="manage_my_account"><i class="fa fa-cog"></i> Manage Account</a>
+            <a class="dropdown-item" href="ajax.php?action=logout"><i class="fa fa-power-off"></i> Logout</a>
+          </div>
+        </div>
       </div>
-
-  <!-- Account Settings Dropdown (Login Name) -->
-  <div class="dropdown">
-    <a href="#" class="text-white dropdown-toggle" id="account_settings" data-toggle="dropdown"
-      aria-haspopup="true" aria-expanded="false">
-      <?php echo $_SESSION['login_name']; ?>
-    </a>
-    <div class="dropdown-menu" aria-labelledby="account_settings" style="left: -2.5em;">
-      <a class="dropdown-item" href="javascript:void(0)" id="manage_my_account"><i class="fa fa-cog"></i> Manage Account</a>
-      <a class="dropdown-item" href="ajax.php?action=logout"><i class="fa fa-power-off"></i> Logout</a>
-    </div>
-  </div>
-</div>
 
     </div>
 
 </nav>
 
 <script>
-  $('#manage_my_account').click(function () {
+  $('#manage_my_account').click(function() {
     uni_modal("Manage Account", "manage_user.php?id=<?php echo $_SESSION['login_id'] ?>&mtype=own")
   })
 </script>
